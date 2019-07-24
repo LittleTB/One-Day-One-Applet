41d4bc5bbd952476e8b96eb4e0833edddc5a113d
LittleTB <pengyouwei1997@gmail.com>
2019-07-24 17:31:14 +0800

    add commit for 0001.py

--- 0001.py
+++ 0001.py
@@ -5,6 +5,6 @@ squ='qwertyuiopasdfghjklzxcvbnml234S67890'
 for i in range(200):
 	codes = []
 	for i in range(5):
-		code = ''.join(random.sample(squ.upper(), 5))
+		code = ''.join(random.sample(squ.upper(), 5)) # 从squ字符串中随机取5个字符组成字符串
 		codes.append(code)
 	print('-'.join(codes))
